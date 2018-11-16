bb6177ccc5778faddd3170aa0e47ae25b62a7114
Mat Dupont <dupont.mat.l@gmail.com>
2018-11-15 22:01:11 -0500

    added offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,16 +28,17 @@ module.exports = {
     'gatsby-plugin-styled-components',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-offline',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Woodward Bicycle Co.',
+        short_name: 'WBCo',
         start_url: '/',
-        background_color: '#663399',
+        background_color: '#FFFFFF',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/WBCo.logo.crown.svg', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
