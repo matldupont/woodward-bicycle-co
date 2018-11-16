deeac690c55d2100d39ba9e5f8e79a57d02936f5
Mat Dupont <dupont.mat.l@gmail.com>
2018-11-15 23:21:09 -0500

    removed template page 2 and adjusted styling on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import styled from 'styled-components';
 import Layout from '../components/layout'
+
+import { Link } from 'gatsby';
+
+const NotFoundContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 3rem;
+`;
+
+const NotFoundHeading = styled.h1`
+  font-size: 2rem;
+`;
+
+const NotFoundText = styled.p`
+  font-size: 1.4rem;
+`;
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <NotFoundContainer>
+      <NotFoundHeading>NOT FOUND</NotFoundHeading>
+      <NotFoundText>You just hit a route that doesn&#39;t exist... <Link to="/">Go back</Link></NotFoundText>
+    </NotFoundContainer>
+    
   </Layout>
 )
 
