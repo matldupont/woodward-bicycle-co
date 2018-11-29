1c97653c3902a540cf7eaebecb4180eec5b53817
Mat Dupont <dupont.mat.l@gmail.com>
2018-11-28 22:00:21 -0500

    added manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,18 +29,18 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     // 'gatsby-plugin-offline',
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: 'Woodward Bicycle Co.',
-    //     short_name: 'WBCo',
-    //     start_url: '/',
-    //     background_color: '#FFFFFF',
-    //     theme_color: '#663399',
-    //     display: 'minimal-ui',
-    //     icon: 'src/images/WBco-logo.jpg', // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Woodward Bicycle Co.',
+        short_name: 'WBCo',
+        start_url: '/',
+        background_color: '#FFFFFF',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/WBco-logo.jpg', // This path is relative to the root of the site.
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
