1ed848d33d4059d0c3416b8ec02a2146c4814648
Mat Dupont <dupont.mat.l@gmail.com>
2018-11-28 21:58:10 -0500

    testing pwa removal

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,19 +28,19 @@ module.exports = {
     'gatsby-plugin-styled-components',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    'gatsby-plugin-offline',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'Woodward Bicycle Co.',
-        short_name: 'WBCo',
-        start_url: '/',
-        background_color: '#FFFFFF',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/WBco-logo.jpg', // This path is relative to the root of the site.
-      },
-    },
+    // 'gatsby-plugin-offline',
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: 'Woodward Bicycle Co.',
+    //     short_name: 'WBCo',
+    //     start_url: '/',
+    //     background_color: '#FFFFFF',
+    //     theme_color: '#663399',
+    //     display: 'minimal-ui',
+    //     icon: 'src/images/WBco-logo.jpg', // This path is relative to the root of the site.
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
