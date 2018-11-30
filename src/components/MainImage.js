add5f2fde86954d52abeb51266b73cba10385135
Mat Dupont <dupont.mat.l@gmail.com>
2018-11-29 19:06:08 -0500

    pwa

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -20,7 +20,7 @@ const MainImage = () => (
         placeholderImage: file(relativePath: { eq: "main.jpg" }) {
           childImageSharp {
             fluid {
-              ...GatsbyImageSharpFluid
+              ...GatsbyImageSharpFluid_tracedSVG
             }
           }
         }
