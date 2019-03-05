da5f2c6eea1646cecefd4f4f77ee48d4235d18e8
Mat Dupont <dupont.mat.l@gmail.com>
2019-03-04 22:01:11 -0500

    set up about page preview

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -2,9 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { AboutPageTemplate } from '../../templates/about-page'
 
-const AboutPagePreview = ({ entry, getAsset }) => {
-  console.log('entry', entry)
-
+const AboutPagePreview = ({ entry }) => {
   // const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
   // const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
 
@@ -14,7 +12,18 @@ const AboutPagePreview = ({ entry, getAsset }) => {
   // const entryPricingPlans = entry.getIn(['data', 'pricing', 'plans'])
   // const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
 
-  return <AboutPageTemplate />
+  return (
+    <AboutPageTemplate
+      heading={entry.getIn(['data', 'heading'])}
+      paragraph1={entry.getIn(['data', 'paragraph1'])}
+      paragraph2={entry.getIn(['data', 'paragraph2'])}
+      paragraph3={entry.getIn(['data', 'paragraph3'])}
+      paragraph4={entry.getIn(['data', 'paragraph4'])}
+      roundImage={entry.getIn(['data', 'roundImage'])}
+      bannerImage={entry.getIn(['data', 'bannerImage'])}
+      fullImage={entry.getIn(['data', 'fullImage'])}
+    />
+  )
 }
 
 AboutPagePreview.propTypes = {
