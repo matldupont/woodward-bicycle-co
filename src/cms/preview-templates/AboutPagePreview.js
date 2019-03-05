ce67e0eddbeabee40cf2b1b29bc48015c1983914
Mat Dupont <dupont.mat.l@gmail.com>
2019-03-04 21:56:09 -0500

    added cms preview files

---
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { AboutPageTemplate } from '../../templates/about-page'
+
+const AboutPagePreview = ({ entry, getAsset }) => {
+  console.log('entry', entry)
+
+  // const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
+  // const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
+
+  // const entryTestimonials = entry.getIn(['data', 'testimonials'])
+  // const testimonials = entryTestimonials ? entryTestimonials.toJS() : []
+
+  // const entryPricingPlans = entry.getIn(['data', 'pricing', 'plans'])
+  // const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
+
+  return <AboutPageTemplate />
+}
+
+AboutPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  getAsset: PropTypes.func,
+}
+
+export default AboutPagePreview
